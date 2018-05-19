8911e278d8f1fcafe664dd6604b7f7d7bc0ae1cf
Stepanov Nikolay <Stepanovnikolaj30@gmail.com>
2018-05-19 22:20:38 +0900

    add new block

---
+++ jquery/map.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.9386300	, 30.3141300],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark([59.8386300	, 30.3141300], {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/icon/map-marker.svg',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+        myPlacemark2 = new ymaps.Placemark([59.9386300	, 30.3141300], {
+          hintContent: 'Собственный значок метки',
+          balloonContent: 'Это красивая метка'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: '../img/icon/map-marker.svg',
+          // Размеры метки.
+          iconImageSize: [30, 42],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38]
+      });
+     
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemark2)
+        myMap.behaviors.disable('scrollZoom');
+        
+});
+})
